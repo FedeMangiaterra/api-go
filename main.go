aceee8287f7019636f3735341441c19d716a81b7
Federico Mangiaterra <ffeede.m@hotmail.com>
2025-01-26 02:14:34 -0300

    endpoint user

--- main.go
+++ main.go
@@ -10,20 +10,31 @@ import (
 )
 
 type User struct {
-	ID    int    `json:"id"`
+	ID    string `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
 	Age   int    `json:"age"`
 }
 
 var users = []User{
-	{ID: 1, Name: "Lionel Messi", Email: "messi@hotmail.com", Age: 37},
-	{ID: 2, Name: "Egg Acuna", Email: "huevo@gmail.com", Age: 33},
-	{ID: 3, Name: "Father Gomez", Email: "papu@gmail.com", Age: 36},
+	{ID: "1", Name: "Lionel Messi", Email: "messi@hotmail.com", Age: 37},
+	{ID: "2", Name: "Egg Acuna", Email: "huevo@gmail.com", Age: 33},
+	{ID: "3", Name: "Father Gomez", Email: "papu@gmail.com", Age: 36},
 }
 
 func GetUsers(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, users)
+}
+
+func GetUser(c *gin.Context) {
+	id := c.Param("id")
+	for _, user := range users {
+		if user.ID == id {
+			c.IndentedJSON(http.StatusOK, user)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
 
 func main() {
@@ -36,6 +47,7 @@ func main() {
 	port := os.Getenv("PORT")
 
 	router.GET("/users", GetUsers)
+	router.GET("/users/:id", GetUser)
 
 	err = router.Run(port)
 	if err != nil {
