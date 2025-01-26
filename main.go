3483456a4a955dd8ebf36765c80299efe581a3a1
Federico Mangiaterra <ffeede.m@hotmail.com>
2025-01-26 02:04:30 -0300

    endpoint users

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+var users = []User{
+	{ID: 1, Name: "Lionel Messi", Email: "messi@hotmail.com", Age: 37},
+	{ID: 2, Name: "Egg Acuna", Email: "huevo@gmail.com", Age: 33},
+	{ID: 3, Name: "Father Gomez", Email: "papu@gmail.com", Age: 36},
+}
+
+func GetUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
 
 func main() {
 	err := godotenv.Load()
@@ -16,6 +34,8 @@ func main() {
 
 	router := gin.Default()
 	port := os.Getenv("PORT")
+
+	router.GET("/users", GetUsers)
 
 	err = router.Run(port)
 	if err != nil {
